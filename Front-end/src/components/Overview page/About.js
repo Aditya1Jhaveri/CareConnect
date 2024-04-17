38b9bd045a01fb30ad6303059bba71ea793f870c
Aditya1Jhaveri <aditya1jhaveri@gmail.com>
2024-04-17 09:20:39 +0530

    Change in About page

--- Front-end/src/components/Overview page/About.js
+++ Front-end/src/components/Overview page/About.js
@@ -1,82 +1,88 @@
 import React from "react";
 import groupdoc from "../../images/Aboutus.jpg";
-import "./About.css";
-import { Card } from "react-bootstrap";
-// import { WidthFull } from "@mui/icons-material";
-import Footer from "./Footer/Footer";
+import "./About.css"; // Import your CSS file for additional styling
+import { Card, Button } from "react-bootstrap";
 
 export const About = () => {
   return (
-    <div>
-      {/* <div className="about">
-      <img src={groupdoc} alt=""/>
-      </div> */}
-
-      {/* <Card.Body style={{ textAlign: 'center' }}>
-        <Card.Text>
-          We are an Online Clinic Appointment System which is designed to bring patients closer to doctors and empower them with a simple but powerful technological device - the smartphone 
-          These apps can help patients connect with doctors instantly, share reports and get prescriptions from doctor and Patients can book Appointment from therir home easily from therir Smartphones with the use of our App.
-          Patient can book the appointment from there home by picking up the date and timmingh slots from there Mobile Phones 
-          A patient can quickly locate the nearest doctor in his/her locality and book an appointment for a direct consultation.
-
-         He/She can:
-
-         Search by City/Specialization/Doctor to view the doctor details and book an appointment online
-         Get notified with appointment status updates (Booking/Rescheduling/Cancellations) by eMail/SMS.
-       </Card.Text>
-      </Card.Body> */}
-
-      <div>
-        <Card
-          style={{
-            width: "110rem",
-            border: "none",
-            boxShadow: "5px 5px 5px lightgrey",
-            // height:  '50rem'
-          }}
-        >
-          <Card.Body style={{ textAlign: "center" }}>
-            <img src={groupdoc} alt="" width={1000} />
-            <h3>
-              <b>About Care Connect</b>
-            </h3>
-            <li>
-              We are <b>Care Connect</b> #1 online Clinic Appointment system.
-            </li>
-            <li>
-              Flexible booking settings allow you to have complete control over
-              your schedule.
-            </li>
-            <li>You can Boook the Appointment Anywhere You are.</li>
-            <li>
-              We are an Online Clinic Appointment System which is designed to
-              bring patiens closer to doctors and empower them with a simple but
-              powerful technological device - the smartphone These apps can help
-              patients connect with doctors instantly, share reports and get
-              prescriptions from doctor and Patients can book Appointment from
-              therir home easily from therir Smartphones with the use of our
-              App. Patient can book the appointment from there home by picking
-              up the date and timmingh slots from there Mobile Phones A patient
-              can quickly locate the nearest doctor in his/her locality and book
-              an appointment for a direct consultation.He/She can: Search by
-              City/Specialization/Doctor to view the doctor details and book an
-              appointment online Get notified with appointment status updates
-              (Booking/Rescheduling/Cancellations) by eMail/SMS. Discover the
-              easiest way to schedule appointments with the #1 online Clinic
-              Appointment system Save time spent on coordinating appointments
-              over phone and email with an all-in-one appointment booking
-              software.
-            </li>
-            <li>
-              200,000+ customers trust <b>Care Connect</b> for their appointment
-              scheduling needs
-            </li>
-            <li>Your peace of mind is our top priority!</li>
-          </Card.Body>
-        </Card>
+    <div className="about-container">
+      <div className="about-image">
+        <img src={groupdoc} alt="About Us" />
       </div>
 
-      <Footer></Footer>
+      <Card className="about-card">
+        <Card.Body className="about-content">
+          <h3 className="about-title">
+            <b>About Care Connect</b>
+          </h3>
+          <p className="about-description">
+            We are <b>Care Connect</b>, the #1 online Clinic Appointment system,
+            connecting patients with expert doctors seamlessly.
+          </p>
+          <ul className="about-list">
+            <li>
+              Flexible booking settings for complete control over your schedule.
+            </li>
+            <li>
+              Book appointments anywhere, anytime using our user-friendly
+              platform.
+            </li>
+            <li>
+              Connect with doctors instantly, share reports, and get
+              prescriptions through our smartphone app.
+            </li>
+            <li>
+              Search by City/Specialization/Doctor and receive appointment
+              status updates via email/SMS.
+            </li>
+            <li>
+              Trusted by over 200,000 customers for their appointment scheduling
+              needs.
+            </li>
+          </ul>
+          <p className="about-mission">
+            Our mission is to revolutionize the healthcare experience by making
+            it convenient and accessible for everyone. We understand the
+            importance of timely medical care, and that's why we've designed
+            Care Connect to be your reliable partner in managing your health.
+          </p>
+          <p>
+            Whether you need to see a specialist or schedule a routine check-up,
+            Care Connect simplifies the process, saving you time and ensuring
+            you get the care you deserve.
+          </p>
+          <h4 className="about-why">Why Choose Care Connect?</h4>
+          <ul className="about-why-list">
+            <li>
+              Expert Doctors: Access a network of highly qualified and
+              experienced healthcare professionals.
+            </li>
+            <li>
+              Convenient Booking: Schedule appointments from the comfort of your
+              home or on-the-go.
+            </li>
+            <li>
+              Secure Communication: Safely share medical reports and receive
+              prescriptions online.
+            </li>
+            <li>
+              Personalized Care: Receive tailored recommendations and reminders
+              for follow-ups.
+            </li>
+            <li>
+              24/7 Support: Our dedicated support team is available
+              round-the-clock to assist you.
+            </li>
+          </ul>
+          <Button
+            variant="primary"
+            href="/AppointmentForm"
+            className="about-button"
+          >
+            Start Booking Now
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
